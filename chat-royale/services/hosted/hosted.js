be31a51662d1495ee1fcf97c9ec633d725f96f21
Jason <codejme@gmail.com>
2020-05-18 16:25:53 -0700

    OAUTH and helix working on AWS
    helixRequest && requestOauth working in hosted.js on aws lambda

--- chat-royale/services/hosted/hosted.js
+++ chat-royale/services/hosted/hosted.js
@@ -13,6 +13,35 @@ const verifyAndDecode = (auth) => {
       return { err: 'Invalid JWT' };
     }
 };
+const requestOauth = async () =>{
+    const link = "https://id.twitch.tv/oauth2/token?client_id=" + process.env.clientId + "&client_secret=" + process.env.clientSecret + "&grant_type=client_credentials"
+    try {
+        const response = await axios.post(link)
+        console.log(response.data, link);
+        // console.log(response.data.explanation);
+        return response.data.access_token
+    } catch (error) {
+        console.log(error.response.body, link);
+        return error
+    }
+}
+const helixRequest = async (names) =>{
+    const link = "https://api.twitch.tv/helix/users?" + names;
+    const oauth = await requestOauth()
+    try {
+        const response = await axios.get(link,{
+            headers:{
+                'Authorization': 'Bearer ' + oauth,
+                'Client-ID' : process.env.clientId
+            }
+        })
+        console.log(response.data, link);
+        return response.data.access_token
+    }catch (error) {
+        console.log(error.response.body);
+        return error
+    }
+}
 const tmiRequest = async () => {
     // console.log("promise begin");
     link = 'https://tmi.twitch.tv/group/user/itmejp/chatters'
@@ -26,9 +55,35 @@ const tmiRequest = async () => {
         return error
     }
 };
-const getViewerHandler = (channelId) =>{
-    viewers = tmiRequest()
-    return viewers
+const getViewerHandler = async (channelId) =>{
+    const viewers = await tmiRequest()
+    const royale_count = Math.min(24, viewers.length)
+    var hold = []
+    var pointer = 0
+    for(var i = 0; i<royale_count; i++){
+      var min = 0
+      var max = data.length
+      pointer = Math.floor(Math.random() * (max - min + 1)) + min
+      hold.push(data[pointer])
+      data.splice(pointer,1)
+    }
+    hold.unshift("tempo")
+    hold.unshift('trihex')
+    var names = ''
+    var pairs = []
+    for(item in hold){
+        if(names === ''){
+          names = 'login=' + hold[item]
+        }
+        else{
+          names = names + "&login=" + hold[item]
+        }
+    }
+    info = await helixRequest(names)
+    for(var item in info){
+        pairs.push([info[item].id,hold[item]])
+      }
+    return info
 }
 
 exports.handler = async event => {
