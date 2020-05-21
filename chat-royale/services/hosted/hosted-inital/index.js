5d440c738aa3a4890a79d6ddc58197786e0de133
Jason <codejme@gmail.com>
2020-05-21 12:09:23 -0700

    returns string for initial extension state

--- chat-royale/services/hosted/hosted-inital/index.js
+++ chat-royale/services/hosted/hosted-inital/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const AWS = require('aws-sdk');
+var isLive = {}
 const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
 
 const verifyAndDecode = (auth) => {
@@ -20,10 +21,37 @@ const dbQuery = async (channel) =>{
     };
     const channelData = await documentClient.get(params).promise();
     console.log("CHANNEL--", channelData)
-    if(channelData.Item.isActive){
-        return channelData.Item.viewers;
+    if(channelData.Item){
+        if(channelData.Item.isActive){
+            isLive[channel] = channelData.Item.viewers
+            return channelData.Item.viewers;
+        }else{
+            return 'false';
+        }
     }else{
         return 'false';
+    }
+}
+const removeQuery = async (channel) => {
+    const start = isLive[channel];
+    var hold = []
+    var params = {
+        TableName: 'chat-royale-2',
+        KeyConditionExpression: 'channel = :channel',
+        ExpressionAttributeValues: {
+            ':channel': channel
+        }
+    }
+    try{
+        const chData = await documentClient.query(params).promise();
+        console.log("LOGGING---", chData)
+        for(var item in chData.Items){
+            hold.push(chData.Items[item]['removed'])
+        }
+        console.log("hold",hold)
+        return hold
+    }catch(err){
+        console.log('err', err)
     }
 }
 exports.handler = async event => {
@@ -36,7 +64,21 @@ exports.handler = async event => {
       return { statusCode, body: JSON.stringify(body, null, 2), headers };
     };
     const payload = verifyAndDecode(event.headers.Authorization);
-    const ping = await dbQuery(payload.channel_id)
-    return respond('200', ping)
+    try{
+        if(isLive[payload.channel_id]){
+            console.log('shortcut')
+            const res = await removeQuery(payload.channel_id);
+            var message = "contestansAndRemoves--" + isLive[payload.channel_id].toString() + "&&" + res.toString()
+            return respond('200', message)
+        }else{
+            const ping = await dbQuery(payload.channel_id)
+            const res = await removeQuery(payload.channel_id)
+            var message = "contestansAndRemoves--" + isLive[payload.channel_id].toString() + "&&" + res.toString()
+            return respond('200', message)
+        }
+    }catch(err){
+        // console.log('catch triggered',err)
+        return respond('200', 'falses')
+    }
     
 }
